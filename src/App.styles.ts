e5fdfd398a425848c25a5bd5b25c877f4ab91154
git-bia19 <56919489+git-bia19@users.noreply.github.com>
2020-08-19 16:00:19 +0400

    changed bkground

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,5 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import BGImage from './images/pixelated.gif';
+import BGImage from './images/some-waneella.gif';
 
 export const GlobalStyle = createGlobalStyle`
     html {
